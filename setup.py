2e5a73c04d9269d492e5d0519ddf001a4c1b28ad
Hanno Schlichting <hanno@hannosch.eu>
2010-07-10 13:13:49 +0000

    Boilerplate

---
+++ setup.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(name='Products.PythonScripts',
+      version = '2.13.0dev',
+      url='http://pypi.python.org/pypi/Products.PythonScripts',
+      license='ZPL 2.1',
+      description="",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=open('README.txt').read() + '\n' +
+                       open('CHANGES.txt').read(),
+      packages=find_packages('src'),
+      namespace_packages=['Products'],
+      package_dir={'': 'src'},
+      install_requires=[
+        'setuptools',
+        'Zope2 >= 2.13.0a1',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
