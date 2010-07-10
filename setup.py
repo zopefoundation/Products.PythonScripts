7489a36d1f97af56dc424c66d16f51db07b812d8
Hanno Schlichting <hanno@hannosch.eu>
2010-07-10 13:25:27 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.PythonScripts',
-      version = '2.13.0',
+      version = '2.13.1dev',
       url='http://pypi.python.org/pypi/Products.PythonScripts',
       license='ZPL 2.1',
       description="Provides support for restricted execution of Python "
