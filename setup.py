706b36053e4f0806ee86de9ec7f4867a1d586944
Tres Seaver <tseaver@palladion.com>
2015-06-18 22:41:43 -0400

    Fix version number.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.PythonScripts',
-      version = '2.13.3dev',
+      version = '2.13.3.dev0',
       url='http://pypi.python.org/pypi/Products.PythonScripts',
       license='ZPL 2.1',
       description="Provides support for restricted execution of Python "
