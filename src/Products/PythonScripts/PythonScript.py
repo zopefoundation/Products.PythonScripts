28e3c620d51b080c00eafe0357aabb82e8415129
Martin Häcker <mhaecker@mac.com>
2018-05-22 18:27:58 +0200

    Switch to FontAwesome icons

--- src/Products/PythonScripts/PythonScript.py
+++ src/Products/PythonScripts/PythonScript.py
@@ -106,7 +106,7 @@ class PythonScript(Script, Historical, Cacheable):
     """
 
     meta_type = 'Script (Python)'
-    zmi_icon = 'zmi-icon-python'
+    zmi_icon = 'fab fa-python'
     _proxy_roles = ()
 
     _params = _body = ''
