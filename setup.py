1a990f8bce285a38ee28d2d7afeb403d5597baf6
Jens Vagelpohl <jens@netz.ooo>
2019-04-15 09:45:25 -0500

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 
 setup(name='Products.PythonScripts',
-      version='4.6',
+      version='4.7.dev0',
       url='https://github.com/zopefoundation/Products.PythonScripts',
       project_urls={
           'Issue Tracker': ('https://github.com/zopefoundation/'
